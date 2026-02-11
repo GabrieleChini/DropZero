80d136b37deb79ab789f9f2d045f4ae2f2f9c684
Tiziano <mtiziano05@gmail.com>
2026-02-11 22:00:47 +0100

    change: Restore branch to previous githun state

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUserProfile, updateUserProfile } = require('../controllers/userController');
-
-router.get('/profile/:userId', getUserProfile);
-router.put('/profile/:userId', updateUserProfile);
-
-module.exports = router;
